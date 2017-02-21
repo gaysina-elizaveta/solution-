ae53fbce3df2900488b6b9c1654a8d80356f0b8d
gaysina-elizaveta <gaysina.elizaveta@gmail.com>
2017-02-21 10:49:23 +0500

    5

--- ConsoleApplication24/Program.cs
+++ ConsoleApplication24/Program.cs
@@ -11,6 +11,11 @@ namespace ConsoleApplication23
 
         static void Main(string[] args)
         {
+            double p = 0;
+            double ar = 0;
+            int count1 = 0;
+            int count2 = 0;
+
             Traingle[] trgl = new Traingle[10];
             Random gen1 = new Random();
             for (int i = 0; i < trgl.Length; i++)
@@ -19,9 +24,7 @@ namespace ConsoleApplication23
                 Random gen = new Random();
                 for (int  j= 0; j < 3; j++)
                 {
-
                     point[j] = new Point(gen.Next(10), gen.Next(10));
-
                 }
                 trgl[i] = new Traingle(point[0], point[1], point[2]);
 
@@ -31,14 +34,27 @@ namespace ConsoleApplication23
                 Console.WriteLine("Площадь треугольника равна:" + trgl[i].Area());
                 Console.WriteLine("Вид треугольника:" + trgl[i].GetType());
 
-                if (trgl[i].GetType )
+                if (trgl[i].GetType() == "прямоугольный" )
                 {
-
+                    count1++;
+                    p = p + trgl[i].Perimeter();
                 }
+                if (trgl[i].GetType() == "равнобедренный")
+                {
+                    count2++;
+                    ar =  ar + trgl[i].Area();
+                }
+
             }
 
+            p = p / count1;
 
-            Console.ReadKey();
+            ar = ar / count2;
+
+            Console.WriteLine("Средний периметр всех прямоугольных треугольников равен:", p);
+            Console.WriteLine("Средняя площадь всех равнобедренных треугольников равна:", ar);
+
+            Console.Read();
         }
 
         
